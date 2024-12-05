e70411339493450217aaad604d0986f63e2a1f01
Muhammad Usama <m.usama.liaqat@gmail.com>
2024-12-05 16:15:35 +0500

    Initial Commit

---
+++ Camera2RTSP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera2RTSP
+//
+//  Created by Usama Liaqat on 05/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
